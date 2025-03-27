b8ca2d20418161437ef7616611b7dd141243a68c
Maxim Pricop <maxim.meson@gmail.com>
2025-03-27 21:06:36 +0200

    fix(web): vite config now includes files outside web directory
    Set the public directory in vite config to the root of the repository so that external static files like the ones in the static folder or in locales will be useable by vite and the web.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
 			"@common": path.resolve(__dirname, "../src/common")
 		}
 	},
+	publicDir: "../",
 	build: {
+		copyPublicDir: false,
 		outDir: "../dist/web",
 		emptyOutDir: true,
 		rollupOptions: {
