5af96755c1e9b5283a3fce8f1efa7c8b1f6534bb
Maxim Pricop <maxim.meson@gmail.com>
2025-03-27 23:56:19 +0200

    refactor(web): added the type to the count state

--- web/src/components/counter.tsx
+++ web/src/components/counter.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./counter.css";
 
 function Counter() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState<number>(0);
 
 	return (
 		<div className="boilerplate-counter">
