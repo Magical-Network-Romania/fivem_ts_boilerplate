929402ea1136dc46dbfe42be68f5364a5d1b27f0
Max <64973789+maxpricop@users.noreply.github.com>
2025-07-17 03:14:48 +0300

    feat(web): added css modules to counter

--- web/src/components/counter.tsx
+++ web/src/components/counter.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
-import "./counter.css";
 import { getLocale } from "~/localization";
+import styles from "./counter.module.css";
 
 function Counter() {
 	const [count, setCount] = useState<number>(0);
 
 	return (
-		<div className="boilerplate-counter">
-			<h3>Boilerplate Counter, {getLocale("hello")}</h3>
-			<p>Count: {count}</p>
+		<div>
+			<h3 className={styles.title}>
+				Boilerplate Counter, {getLocale("hello")}
+			</h3>
+			<p className={styles.text}>Count: {count}</p>
 
-			<div className="counter-buttons">
+			<div className={styles.buttons}>
 				<button type="button" onClick={() => setCount((prev) => ++prev)}>
 					Increment
 				</button>
