c0ae607267235137df1528d3e0e1f474a20d04d8
Max <64973789+maxpricop@users.noreply.github.com>
2025-07-19 01:58:43 +0300

    docs: added JSDoc to isEnvBrowser function

--- web/src/utils/browser.ts
+++ web/src/utils/browser.ts
@@ -1,5 +1,10 @@
 import { resourceEnv } from "@shared/resource";
 
+/**
+ * Determines if the current environment is a browser or the FiveM CEF.
+ *
+ * @returns {boolean} True if the environment is a browser, false if it is the FiveM CEF.
+ */
 export function isEnvBrowser(): boolean {
 	// const isCfxNui: boolean = window.location.href.startsWith("https://cfx-nui-");
 	// const isNuiProtocol: boolean = window.location.protocol === "nui:";
