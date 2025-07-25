b27db8415308e4430011f5608490aeb1c7d33964
Max <64973789+maxpricop@users.noreply.github.com>
2025-07-26 02:28:45 +0300

    refactor(web): the page icon is set from html

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,4 @@
-import pageIcon from "@assets/web/vite.svg";
-import { type JSX, useEffect, useState } from "react";
+import { type JSX, useState } from "react";
 import Counter from "./components/counter";
 import { useNuiEvent } from "./hooks/useNuiEvent";
 import { isEnvBrowser } from "./utils/browser";
@@ -10,19 +9,6 @@ function App(): JSX.Element {
 	useNuiEvent<boolean>("setVisible", (data) => {
 		setVisible(data);
 	});
-
-	useEffect(() => {
-		let link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']");
-
-		if (!link) {
-			link = document.createElement("link");
-			link.rel = "icon";
-			document.head.appendChild(link);
-		}
-
-		link.href = pageIcon;
-		link.type = "image/svg+xml";
-	}, []);
 
 	return <>{visible && <Counter />}</>;
 }
